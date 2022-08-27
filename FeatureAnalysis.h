d0ca70a4c84253eecbcbd84462c161a7b2b531bb
wzx <59754483+Zuoxun@users.noreply.github.com>
2022-08-27 22:37:22 +0800

    建立完整索引表+圆柱圆孔特征提取+代码结构优化

--- FeatureAnalysis.h
+++ FeatureAnalysis.h
@@ -8,4 +8,4 @@ double pointDistance(map<int, CARTESIAN_POINT>::iterator it1, map<int, CARTESIAN
 double pointDistance(double x1, double y1, double z1, double x2, double y2, double z2);
 
 //提取圆柱和圆孔
-void cylindrical();
+void cylinder_and_circularHole();
